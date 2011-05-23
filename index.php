c6024f698faf5c5c031e7d4bddaef000342338c8
Andrew Peng <pengc99@gmail.com>
2011-05-23 01:17:17 -0700

    added comments in code

--- index.php
+++ index.php
@@ -30,11 +30,14 @@
 	GNU General Public License for more details.
 	*/
 
+	//checks to see if the form is submitted to itself
 	if(isset($_FILES["uploadFiles"]))
 	{
+		//checks to see if the data directory exists
 		$uploaded = false;
 		if (count(glob($_SERVER['DOCUMENT_ROOT']."/files/*", GLOB_ONLYDIR)) == 0)
 		{
+			//creates the data directory structure
 		        touch($_SERVER['DOCUMENT_ROOT']."/files/index.html");
 		        $characters = "0123456789abcdef";
 		        for($i = 1; $i <= strlen($characters); $i++)
@@ -44,16 +47,22 @@
 		                touch($_SERVER['DOCUMENT_ROOT']."/files/".$newUUID."/index.html");
 		        }
 		}
+
+		//loop for each file to be uploaded 
 		foreach ($_FILES["uploadFiles"]["error"] as $key => $error) 
 		{
+			//only begin processing if upload completes without error
 			if (($error == UPLOAD_ERR_OK) && ($_FILES['uploadFiles']['type'][$key] == "image/jpeg" || $_FILES['uploadFiles']['type'][$key] == "image/gif" || $_FILES['uploadFiles']['type'][$key] == "image/png"))
 			{
+				//set variables and stuff
 				$uploaded = true;
 				$uploadDir = "files/";
 				$fileType = $_FILES['uploadFiles']['type'][$key];
 				$fileTmpLoc = $_FILES["uploadFiles"]["tmp_name"][$key];
 				$fileName = $_FILES["uploadFiles"]["name"][$key];
+				//generate the crc32 checksum of the file
 				$fileHash = hash_file(crc32,$fileTmpLoc);
+				//determine filetyes to prepare for renaming and tokenizing of the filenames
 				switch ($fileType)
 				{
 					case "image/jpeg":
@@ -66,18 +75,28 @@
 	                                        $fileExt = "gif";
 	                                break;
 				}
+				//rename the file to it's crc32 checksum and set the extension
 				$fileNewName = $fileHash.".".$fileExt;
 				$globArray = glob($uploadDir.substr($fileHash, 0, 1)."*");
 				$fileNewLoc = $globArray[0]."/".$fileNewName;
 				$fileAbsLoc = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].$fileNewLoc;
+				//location of the full size image
 				$fileLocArray['full'][] = $fileAbsLoc;
+				//move file to it's final location
 				move_uploaded_file($fileTmpLoc, $fileNewLoc);
+				//create imagemagick object for image resizing to make the thumbnail
 				$theImage = new Imagick($fileNewLoc);
+				//this removes EXIF data for privacy concerns
 				$theImage->stripImage();
+				//write the stripped fullsize image
 				$theImage->writeImage($fileNewLoc);
+				//generate a thumbnail 120px side
 				$theImage->thumbnailImage(120,0);
+				//add a black border to the thumbnail image
 				$theImage->borderImage("black",2,2);
+				//write the thumbnail and affix a _th to the file name before the extension
 				$theImage->writeImage($globArray[0]."/".$fileHash."_tn.".$fileExt);
+				//set location of the thumbnail
 				$fileLocArray['thumb'][] = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].$globArray[0]."/".$fileHash."_tn.".$fileExt;
 			}
 		}
