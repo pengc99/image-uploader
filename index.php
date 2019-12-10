25124da2572366b709c26a8422fe1f2f93fe8122
Andrew Peng <pengc99@gmail.com>
2019-12-09 23:49:34 -0600

    Update to include optimizations for GIFs

--- index.php
+++ index.php
@@ -1,152 +1,196 @@
-<?
-	/*
-	Yep. Hosts images. One of the more well-coded and graceful scripts
-	I've written. Takes advantage of the multiple-upload HTML5 element.
-	Each uploaded image is hashed to it's CRC32 hash, and then stored
-	in one of 16 directories created via UUID's. The UUID is generated
-	and then a prefix is applied for each element of the hexadecimal
-	elements (regex [0-9a-f]). This way, we get a more or less uniform
-	distribution of files across 16 directories to reduce the stress
-	on the filesystem if you had them all stuffed in one directory.
-	Should be good for at least several hundred thousand files per
-	instance of the script. The script also tries to be as dynamic and
-	self-healing as possible. If it detects missing data directories
-	it simply recreates it. If you upload a file that already exists
-	it will overwrite the current file. Thumbnails are generated at
-	upload using the PHP ImageMagick PECL library. HTML, phpBB, and
-	direct URL codes are given when images are uploaded.
-	
-	Created and maintained by Andrew Peng:
-	http://andrewpeng.net/computing/php-scripting/image-uploader-script
-	
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
-	the Free Software Foundation, either version 3 of the License, or
-	(at your option) any later version.
-	
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-	GNU General Public License for more details.
-	*/
+<?php
 
-	//checks to see if the form is submitted to itself
-	if(isset($_FILES["uploadFiles"]))
-	{
-		//checks to see if the data directory exists
-		$uploaded = false;
-		if (count(glob($_SERVER['DOCUMENT_ROOT']."/files/*", GLOB_ONLYDIR)) == 0)
-		{
-			//creates the data directory structure
-		        touch($_SERVER['DOCUMENT_ROOT']."/files/index.html");
-		        $characters = "0123456789abcdef";
-		        for($i = 1; $i <= strlen($characters); $i++)
-		        {
-		                $newUUID = substr($characters, ($i-1), 1).uniqid();
-		                mkdir($_SERVER['DOCUMENT_ROOT']."/files/".$newUUID);
-		                touch($_SERVER['DOCUMENT_ROOT']."/files/".$newUUID."/index.html");
-		        }
-		}
+/*
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
-		//loop for each file to be uploaded 
-		foreach ($_FILES["uploadFiles"]["error"] as $key => $error) 
-		{
-			//only begin processing if upload completes without error
-			if (($error == UPLOAD_ERR_OK) && ($_FILES['uploadFiles']['type'][$key] == "image/jpeg" || $_FILES['uploadFiles']['type'][$key] == "image/gif" || $_FILES['uploadFiles']['type'][$key] == "image/png"))
-			{
-				//set variables and stuff
-				$uploaded = true;
-				$uploadDir = "files/";
-				$fileType = $_FILES['uploadFiles']['type'][$key];
-				$fileTmpLoc = $_FILES["uploadFiles"]["tmp_name"][$key];
-				$fileName = $_FILES["uploadFiles"]["name"][$key];
-				//generate the crc32 checksum of the file
-				$fileHash = hash_file(crc32,$fileTmpLoc);
-				//determine filetyes to prepare for renaming and tokenizing of the filenames
-				switch ($fileType)
-				{
-					case "image/jpeg":
-						$fileExt = "jpg";
-					break;
-	                                case "image/png":
-	                                        $fileExt = "png";
-	                                break;
-	                                case "image/gif":
-	                                        $fileExt = "gif";
-	                                break;
-				}
-				//rename the file to it's crc32 checksum and set the extension
-				$fileNewName = $fileHash.".".$fileExt;
-				$globArray = glob($uploadDir.substr($fileHash, 0, 1)."*");
-				$fileNewLoc = $globArray[0]."/".$fileNewName;
-				$fileAbsLoc = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].$fileNewLoc;
-				//location of the full size image
-				$fileLocArray['full'][] = $fileAbsLoc;
-				//move file to it's final location
-				move_uploaded_file($fileTmpLoc, $fileNewLoc);
-				//create imagemagick object for image resizing to make the thumbnail
-				$theImage = new Imagick($fileNewLoc);
-				//this removes EXIF data for privacy concerns
-				$theImage->stripImage();
-				//write the stripped fullsize image
-				$theImage->writeImage($fileNewLoc);
-				//generate a thumbnail 120px side
-				$theImage->thumbnailImage(120,0);
-				//add a black border to the thumbnail image
-				$theImage->borderImage("black",2,2);
-				//write the thumbnail and affix a _th to the file name before the extension
-				$theImage->writeImage($globArray[0]."/".$fileHash."_tn.".$fileExt);
-				//set location of the thumbnail
-				$fileLocArray['thumb'][] = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].$globArray[0]."/".$fileHash."_tn.".$fileExt;
-			}
-		}
-		if($uploaded)
-		{
-	                echo "<strong>BBCode with thumbnail:</strong><br />\n";
-	                echo "<blockquote>\n";
-	                foreach ($fileLocArray['full'] as $key => $value)
-	                {
-	                        echo "[url=\"".$fileLocArray['full'][$key]."\"][img]".$fileLocArray['thumb'][$key]."[/img][/url]<br />\n";
-	                }
-	                echo "</blockquote>\n";
-	                echo "<br />\n";
-	                echo "<strong>HTML with thumbnail:</strong><br />\n";
-	                echo "<blockquote>\n";
-	                foreach ($fileLocArray['full'] as $key => $value)
-	                {
-	                        echo "&lt;a href=\"".$fileLocArray['full'][$key]."\" target=\"_new\"&gt;&lt;img src=\"".$fileLocArray['thumb'][$key]."\"&gt;&lt;/a&gt;<br />\n";
-	                }
-	                echo "</blockquote>\n";
-	                echo "<br />\n";
-			echo "<strong>BBCode:</strong><br />\n";
-			echo "<blockquote>\n";
-			foreach ($fileLocArray['full'] as $value)
-			{
-				echo "[img]".$value."[/img]<br />\n";
-			}
-			echo "</blockquote>\n";
-	                echo "<br />\n";
-			echo "<strong>HTML:</strong><br />\n";
-	                echo "<blockquote>\n";
-	                foreach ($fileLocArray['full'] as $value)
-	                {
-	                        echo "&lt;img src=".$value."&gt;<br />\n";
-	                }
-	                echo "</blockquote>\n";
-			echo "<br />\n";
-	                echo "<strong>Direct URL:</strong><br />\n";
-	                echo "<blockquote>\n";
-	                foreach ($fileLocArray['full'] as $value)
-	                {
-	                        echo $value."<br />\n";
-	                }
-	                echo "</blockquote>\n";
-		        echo "<hr>\n";
-		}
-	}
-	echo "It doesn't get any easier than this! Welcome to <strong>derp-o-matic</strong> 2000!<br /> <br />\n";
-	echo "<form method=\"post\" action=\"\" enctype=\"multipart/form-data\">\n";
-	echo "<input name=\"uploadFiles[]\" id=\"filesToUpload\" type=\"file\" multiple=\"\"/>\n";
-	echo "<input type=\"submit\" value=\"HerpDerp\">\n";
-	echo "</form>";
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+GNU General Public License for more details.
+*/
+
+echo "<meta content=\"True\" name=\"HandheldFriendly\" />";
+echo "<meta content=\"width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;\" name=\"viewport\" />";
+echo "<meta name=\"viewport\" content=\"width=device-width\" />";
+
+if(isset($_FILES["uploadFiles"]))
+{
+        $uploaded = false;
+        if (count(glob($_SERVER['DOCUMENT_ROOT']."/files/*", GLOB_ONLYDIR)) == 0)
+        {
+                touch($_SERVER['DOCUMENT_ROOT']."/files/index.html");
+                $characters = "0123456789abcdef";
+                for($i = 1; $i <= strlen($characters); $i++)
+                {
+                        $newUUID = substr($characters, ($i-1), 1).uniqid();
+                        mkdir($_SERVER['DOCUMENT_ROOT']."/files/".$newUUID);
+                        touch($_SERVER['DOCUMENT_ROOT']."/files/".$newUUID."/index.html");
+                }
+        }
+        foreach ($_FILES["uploadFiles"]["error"] as $key => $error)
+        {
+                $imageMime = mime_content_type($_FILES['uploadFiles']['tmp_name'][$key]);
+                if (($error == UPLOAD_ERR_OK) && ($imageMime == "image/jpeg" || $imageMime == "image/gif" || $imageMime == "image/png" || $imageMime == "video/mp4"))
+                {
+                        $uploaded = true;
+                        $uploadDir = "files/";
+                        $fileTmpLoc = $_FILES["uploadFiles"]["tmp_name"][$key];
+                        $fileName = $_FILES["uploadFiles"]["name"][$key];
+                        $fileHash = hash_file('crc32',$fileTmpLoc);
+                        switch ($imageMime)
+                        {
+                                case "image/jpeg":
+                                        $fileExt = "jpg";
+                                break;
+
+                                case "image/png":
+                                        $fileExt = "png";
+                                break;
+
+                                case "image/gif":
+                                        $fileExt = "gif";
+                                break;
+
+                                case "video/mp4":
+                                        $fileExt = "mp4";
+                                break;
+                        }
+
+                        $fileNewName = $fileHash.".".$fileExt;
+                        $globArray = glob($uploadDir.substr($fileHash, 0, 1)."*");
+                        $fileNewLoc = $globArray[0]."/".$fileNewName;
+                        $fileAbsLoc = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].$fileNewLoc;
+                        $fileLocArray['full'][] = $fileAbsLoc;
+
+                        #Move the file from the tmp location to the storage location
+                        move_uploaded_file($fileTmpLoc, $fileNewLoc);
+
+                        #Only drop into the loop if upload file is an image
+                        if ($imageMime == "image/jpeg" || $imageMime == "image/png")
+                        {
+                                #Generate small thumbnail
+                                $theImage = new Imagick($fileNewLoc);
+                                $theImage->stripImage();
+                                $theImage->writeImage($fileNewLoc);
+                                $theImage->thumbnailImage(120,0,FALSE);
+                                $theImage->borderImage("black",2,2);
+                                $theImage->writeImage($globArray[0]."/".$fileHash."_tn.".$fileExt);
+                                $theImage->destroy();
+
+                                #Generate medium thumbnail
+                                $theImage = new Imagick($fileNewLoc);
+                                $theImage->stripImage();
+                                $theImage->writeImage($fileNewLoc);
+                                $theImage->thumbnailImage(600,0,FALSE);
+                                $theImage->borderImage("black",2,2);
+                                $theImage->writeImage($globArray[0]."/".$fileHash."_md.".$fileExt);
+                                $theImage->destroy();
+
+                                #Attach thumbnail URLs to the response array
+                                $fileLocArray['thumb'][] = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].$globArray[0]."/".$fileHash."_tn.".$fileExt;
+                                $fileLocArray['medium'][] = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].$globArray[0]."/".$fileHash."_md.".$fileExt;
+                        }
+
+                        #Only drop into the loop if upload file is a GIF
+                        if ($imageMime == "image/gif")
+                        {
+                                #First unoptimize the GIF so it can be resized
+                                exec("/usr/bin/gifsicle --batch --unoptimize $fileNewLoc");
+
+                                #Generate small thumbnail and optimize
+                                exec("/usr/bin/gifsicle --optimize=03 --resize-fit-width 120 $fileNewLoc -o $globArray[0]/{$fileHash}_tn.$fileExt");
+
+                                #Generate medium thumbnail and optimize
+                                exec("/usr/bin/gifsicle --optimize=03 --resize-fit-width 600 $fileNewLoc -o $globArray[0]/{$fileHash}_md.$fileExt");
+
+                                #Optimize the original
+                                exec("/usr/bin/gifsicle --batch --optimize=03 $fileNewLoc");
+
+                                #Attach thumbnail URLs to the response array
+                                $fileLocArray['thumb'][] = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].$globArray[0]."/".$fileHash."_tn.".$fileExt;
+                                $fileLocArray['medium'][] = "http://".$_SERVER["SERVER_NAME"].$_SERVER["REQUEST_URI"].$globArray[0]."/".$fileHash."_md.".$fileExt;
+                        }
+
+                        #Only drop into the loop if upload file is a video
+                        if ($imageMime == "video/mp4")
+                        {
+                        }
+                }
+        }
+        if($uploaded)
+        {
+
+                echo "<strong>Image preview:</strong><br />\n";
+                echo "<blockquote>\n";
+                foreach ($fileLocArray['full'] as $key => $value)
+                {
+                        echo "<img src = \"".$fileLocArray['medium'][$key]."\"><br />\n";
+                }
+                echo "</blockquote>\n";
+                echo "<strong>BBCode with thumbnail:</strong><br />\n";
+                echo "<blockquote>\n";
+                foreach ($fileLocArray['full'] as $key => $value)
+                {
+                        echo "[url=".$fileLocArray['full'][$key]."][img]".$fileLocArray['thumb'][$key]."[/img][/url]<br />\n";
+                }
+                echo "</blockquote>\n";
+                echo "<br />\n";
+                echo "<strong>HTML with thumbnail:</strong><br />\n";
+                echo "<blockquote>\n";
+                foreach ($fileLocArray['full'] as $key => $value)
+                {
+                        echo "&lt;a href=\"".$fileLocArray['full'][$key]."\" target=\"_new\"&gt;&lt;img src=\"".$fileLocArray['thumb'][$key]."\"&gt;&lt;/a&gt;<br />\n";
+                }
+                echo "</blockquote>\n";
+                echo "<br />\n";
+                echo "<strong>BBCode with medium sized image:</strong><br />\n";
+                echo "<blockquote>\n";
+                foreach ($fileLocArray['full'] as $key => $value)
+                {
+                        echo "[url=".$fileLocArray['full'][$key]."][img]".$fileLocArray['medium'][$key]."[/img][/url]<br />\n";
+                }
+                echo "</blockquote>\n";
+                echo "<br />\n";
+                echo "<strong>HTML with medium sized image:</strong><br />\n";
+                echo "<blockquote>\n";
+                foreach ($fileLocArray['full'] as $key => $value)
+                {
+                        echo "&lt;a href=\"".$fileLocArray['full'][$key]."\" target=\"_new\"&gt;&lt;img src=\"".$fileLocArray['medium'][$key]."\"&gt;&lt;/a&gt;<br />\n";
+                }
+                echo "</blockquote>\n";
+                echo "<br />\n";
+                echo "<strong>BBCode:</strong><br />\n";
+                echo "<blockquote>\n";
+                foreach ($fileLocArray['full'] as $value)
+                {
+                        echo "[img]".$value."[/img]<br />\n";
+                }
+                echo "</blockquote>\n";
+                echo "<br />\n";
+                echo "<strong>HTML:</strong><br />\n";
+                echo "<blockquote>\n";
+                foreach ($fileLocArray['full'] as $value)
+                {
+                        echo "&lt;img src=".$value."&gt;<br />\n";
+                }
+                echo "</blockquote>\n";
+                echo "<br />\n";
+                echo "<strong>Direct URL:</strong><br />\n";
+                echo "<blockquote>\n";
+                foreach ($fileLocArray['full'] as $value)
+                {
+                        echo "<a href = \"$value\">$value</a><br />\n";
+                }
+                echo "</blockquote>\n";
+                echo "<hr>\n";
+        }
+}
+        echo "It doesn't get any easier than this! Welcome to <strong>derp-o-matic</strong> 2000!<br /> <br />\n";
+        echo "<form method=\"post\" action=\"\" enctype=\"multipart/form-data\">\n";
+        echo "<input name=\"uploadFiles[]\" id=\"filesToUpload\" type=\"file\" accept=\"image/*,video/*\" multiple=\"\"/>\n";
+        echo "<input type=\"submit\" value=\"HerpDerp\">\n";
+        echo "</form>";
 ?>
